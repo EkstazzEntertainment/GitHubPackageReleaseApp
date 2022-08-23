40ceee511e0d60be469a22f96aa29ed4c81f0574
alexanian <alexanyanyuka@gmail.com>
2022-08-23 15:29:05 +0200

    Currently able to execute an SH command using c#. It can add . and commit -m and push.

--- Assets/Scripts/Logic/Release/CommitAndReleaseHandler.cs
+++ Assets/Scripts/Logic/Release/CommitAndReleaseHandler.cs
@@ -1,5 +1,7 @@
 namespace Logic.Release
 {
+    using System;
+    using System.Diagnostics;
     using System.IO;
 
     public class CommitAndReleaseHandler
@@ -7,6 +9,7 @@ namespace Logic.Release
         public static void Release(string path, string version, string commitMessage)
         {
             AddToChangeLogFile(path, version, commitMessage);
+            ExecuteCommitCommand(path, version, commitMessage);
         }
 
         private static void AddToChangeLogFile(string path, string version, string commitMessage)
@@ -15,6 +18,22 @@ namespace Logic.Release
             jsonText += "\n\n";
             jsonText += $"{version}   {commitMessage}";
             File.WriteAllText(path + "CHANGELOG.md", jsonText);
+        }
+
+        private static void ExecuteCommitCommand(string path, string version, string commitMessage)
+        {
+            var firstArg = path;
+            var secondArg = $" 'v{version} released: {commitMessage}'";
+            var arguments = firstArg + secondArg;
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "/Users/yuriialeksanian/Desktop/MyApps/CustomShellScripts/CommitRelease/CommitRelease";
+            p.StartInfo.Arguments = arguments;
+            p.Start();
+
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
         }
     }
 }

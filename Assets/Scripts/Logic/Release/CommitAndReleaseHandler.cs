2757b5532b20ae21166d84532c799ffd6af1cde0
alexanian <alexanyanyuka@gmail.com>
2022-08-23 20:12:43 +0200

    Sending the POST request to create a release.

--- Assets/Scripts/Logic/Release/CommitAndReleaseHandler.cs
+++ Assets/Scripts/Logic/Release/CommitAndReleaseHandler.cs
@@ -1,19 +1,23 @@
 namespace Logic.Release
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.IO;
+    using System.Threading;
     using Helpers;
+    using UnityEngine;
+    using UnityEngine.Networking;
 
     public class CommitAndReleaseHandler
     {
-        public static void Release(string currentVersion, string path, string version, string commitMessage, Action callback)
+        public static void Release(string packName, string currentVersion, string path, string version, string commitMessage, Action callback)
         {
             AddToChangeLogFile(path, version, commitMessage);
             ChangeVersionInPackageFile(path, currentVersion, version);
             ExecuteCommitCommand(path, version, commitMessage, () =>
             {
-                CreateReleaseViaAPI(callback);
+                CreateReleaseViaAPI(callback, packName, version);
             });
         }
 
@@ -54,9 +58,67 @@ namespace Logic.Release
             callback.Invoke();
         }
 
-        private static void CreateReleaseViaAPI(Action callback)
+        private static void CreateReleaseViaAPI(Action callback, string package, string newVersion)
         {
-            callback.Invoke();
+            var url = $"https://api.github.com/repos/EkstazzEntertainment/{package}/releases";
+            
+            WWWForm form = new WWWForm();
+            AddBody(ref form, newVersion);
+            
+            UnityWebRequest uwr = UnityWebRequest.Post(url, form);
+            AddHeadersToRequest(ref uwr, BuildGetHeaders("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"));
+            uwr.SendWebRequest();
+
+            while (!uwr.isDone)
+            {
+                Thread.Sleep(50);
+            }
+
+            if (uwr.result == UnityWebRequest.Result.ConnectionError)
+            {
+                callback.Invoke();
+            }
+            else
+            {
+                callback.Invoke();
+            }
+            
+            uwr.Dispose();
         }
+
+        private static void AddBody(ref WWWForm form, string newVersion)
+        {
+            form.AddField("tag_name", newVersion);
+            form.AddField("target_commitish", "develop");
+            form.AddField("name", newVersion);
+            form.AddField("body", "default");
+            form.AddField("draft", 0);
+            form.AddField("prerelease", 0);
+            form.AddField("generate_release_notes", 0);
+        }
+        
+        private static void AddHeadersToRequest(ref UnityWebRequest uwr, List<Header> headers)
+        {
+            foreach (var header in headers)
+            {
+                uwr.SetRequestHeader(header.Name, header.Value);
+            }
+        }
+        
+        private static List<Header> BuildGetHeaders(string token)
+        {
+            List<Header> headers = new List<Header>
+            {
+                new Header {Name = "Accept", Value = "application/vnd.github+json"},
+                new Header {Name = "Authorization", Value = "token " + token}
+            };
+            return headers;
+        }
+    }
+    
+    public class Header
+    {
+        public string Name;
+        public string Value;
     }
 }
